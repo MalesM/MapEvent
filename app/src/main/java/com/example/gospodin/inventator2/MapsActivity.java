9891920c562d3af6e100ab150c3f9e86e7579de5
MalesM <markommales@gmail.com>
2017-08-10 15:21:35 +0200

    track invents in specific range and notify //fast circle fix

--- app/src/main/java/com/example/gospodin/inventator2/MapsActivity.java
+++ app/src/main/java/com/example/gospodin/inventator2/MapsActivity.java
@@ -420,7 +420,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         long firstMillis = System.currentTimeMillis();
         AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
 
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, 1000*20, pendingIntent);
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, 1000*60*2, pendingIntent);
     }
     public void cancelAlarm() {
         Intent intent = new Intent(getApplicationContext(), MyAlarmReceiver.class);
